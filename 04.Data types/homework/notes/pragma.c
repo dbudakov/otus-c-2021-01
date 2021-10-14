ea17e79d60b3a73a227fac10d04a16c20493dc3a
Dmitrii Budakov <budakov.web@gmail.com>
2021-10-14 08:31:22 +0300

    next

---
+++ 04.Data types/homework/notes/pragma.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#pragma pack(push, 1)
+typedef struct
+{
+    unsigned char x;
+    unsigned short y;
+    unsigned int z;
+} my_arg_t_pragma;
+#pragma pack(pop)
+
+typedef struct
+{
+    unsigned char x;
+    unsigned short y;
+    unsigned int z;
+} my_arg_t_without_pragma;
+
+
+int main(){
+    printf("size of data is %d\n", sizeof(my_arg_t_pragma));
+    printf("size of data is %d\n", sizeof(my_arg_t_without_pragma));
+}
